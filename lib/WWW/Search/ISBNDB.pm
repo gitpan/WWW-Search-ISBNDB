381711c0236edda9d9f3a4ac2978f81fdf49658d
Nicholas Gerakines <nick@socklabs.com>
2006-08-22 16:43:41 +0000

    Import of SOCK/WWW-Search-ISBNDB-0.2 from CPAN.
    gitpan-cpan-distribution: WWW-Search-ISBNDB
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         SOCK/WWW-Search-ISBNDB-0.2.tar.gz
    gitpan-cpan-author:       SOCK
    gitpan-cpan-maturity:     released

--- lib/WWW/Search/ISBNDB.pm
+++ lib/WWW/Search/ISBNDB.pm
@@ -1,25 +1,86 @@
+# $Id: $ $Revision: $ $Source: $ $Date: $
+
 package WWW::Search::ISBNDB;
 
 use strict;
-no warnings qw(redefine);
+use warnings;
 
-use Carp;
 use LWP::UserAgent;
 use WWW::Search qw(generic_option);
 use WWW::SearchResult;
 use XML::Simple;
+
 use vars qw(@ISA $VERSION);
 
 @ISA = qw(WWW::Search);
-$VERSION = "0.1";
+$VERSION = '0.2';
+
+sub url_encode {
+    my ($encode ) = @_;
+    if (! $encode) { return; }
+    $encode =~ s/([^A-Za-z0-9\-_.!~*'() ])/ uc sprintf "%%%02x",ord $1 /xmeg;
+    $encode =~ tr/ /+/;
+    return $encode;
+}
+
+sub native_setup_search {
+	my($self, $query) = @_;
+	if (! $self->{'key'}) { die 'No license key given to WWW::Search::ISBDB!'; }
+	$self->{'_offset'} = 0;
+	$self->{'_query'} = $query;
+	return 1;
+}
+
+sub native_retrieve_some {
+	my ($self) = @_;
+	
+	# HACK: Consider better url encoding.
+    my $query = url_encode( $self->{'_query'} );
+
+	my %args = (
+		'access_key' => $self->{'key'},
+		'index1' => $self->{'_type'} || 'combined',
+		'value1' => $self->{'_query'},
+		'results' => 'details+subjects+texts',
+	);
+	
+	my $url = 'http://isbndb.com/api/books.xml?'.( join q{&},  map { $_ . q{=} . $args{$_} } keys %args );
+
+	my $ua = LWP::UserAgent->new(
+		'agent' => "W3SearchISBNDB/$VERSION",
+	);
+	my $response = $ua->get( $url );
+
+	if (! $response->is_success ) { return; }
+	my $content = $response->content;
+	if ( $content ) {
+		my $xs = new XML::Simple();
+		my $ref = $xs->XMLin("$content" );
+		foreach my $book ( @{ $ref->{'BookList'}{'BookData'} }) {
+			my $hit = WWW::SearchResult->new();
+			$hit->{'book_id'} = $book->{'book_id'};
+			$hit->{'isbn'} = $book->{'isbn'};
+			$hit->{'language'} = $book->{'Details'}{'language'} || q{};
+			$hit->{'summary'} = $book->{'Summary'} || q{};
+			$hit->{'titlelong'} = $book->{'TitleLong'} || q{};
+			$hit->{'notes'} = $book->{'Notes'} || q{};
+			$hit->title( $book->{'Title'} );
+			$hit->url( 'http://isbndb.com/search-all.html?kw=' . $book->{'isbn'} );
+			push @{$self->{cache}}, $hit;
+		}
+	}
+
+	return;
+}
+
+1;
+__END__
+
+=pod
 
 =head1 NAME
 
 WWW::Search::ISBNDB - Search for book information on isbndb.com
-
-=head1 VERSION
-
-Version 0.01
 
 =head1 SYNOPSIS
 
@@ -27,10 +88,12 @@ This module creates an easy to use interface for searching books on isbndb.com.
 
   use WWW::Search;  
   my $search = WWW::Search->new('ISBNDB', key => 'abcd1234');
-  $search->native_query("born in blood");
+  $search->native_query('born in blood');
+  my $result = $search->next_result();
   while (my $result = $search->next_result() ) {
-    print $result->{isbn}.' - '.$result->{book_id}, "\n";
-    print $result->{title}.' - '.$result->url, "\n";
+	print "$result->title ($result->{'isbn'} - $result->{'book_id})\n";
+	print " -- $result->{'titlelong'}\n";
+	print "  '$result->{'summary'}'\n";
   }
 
 =head1 DETAILS
@@ -38,15 +101,6 @@ This module creates an easy to use interface for searching books on isbndb.com.
 =head2 native_setup_search
 
 This prepares the search by checking for a valid developer key. A developer key can be obtained at isbndb.com and is free.
-
-=cut
-
-sub native_setup_search {
-	my($self, $query) = @_;
-	croak("No license key given to WWW::Search::ISBDB!") unless ( $self->{key} && $self->{key} =~ m/^[a-zA-Z0-9]+$/ );
-	$self->{_offset} = 0;
-	$self->{_query} = $query;
-}
 
 =head2 native_retrieve_some
 
@@ -67,58 +121,7 @@ the object hash and consist of the following:
   titlelong
   notes
 
-=cut
-
-sub native_retrieve_some {
-	my ($self) = @_;
-	
-	# HACK: Consider better url encoding.
-	$self->{_query} =~ s/ /%20/g;
-
-	my %args = (
-		'access_key' => $self->{key},
-		'index1' => $self->{_type} || 'combined',
-		'value1' => $self->{_query},
-		'results' => 'details+subjects+texts',
-	);
-	
-	my $url = 'http://isbndb.com/api/books.xml?'.( join( '&',  map { "$_=".$args{$_} } keys %args) );
-
-	my $ua = LWP::UserAgent->new;
-	my $response = $ua->get( $url );
-
-	return unless ( $response->is_success );
-	return unless ( my $content = $response->content );
-
-	if ( $content ) {
-		my $xs = new XML::Simple();
-		my $ref = $xs->XMLin("$content" );
-		foreach my $book ( @{ $ref->{'BookList'}{'BookData'} }) {
-			my $hit = WWW::SearchResult->new();
-			$hit->{'book_id'} = $book->{'book_id'};
-			$hit->{'isbn'} = $book->{'isbn'};
-			$hit->{'language'} = $book->{'Details'}{'language'} || '';
-			$hit->{'summary'} = $book->{'Summary'} || '';
-			$hit->{'titlelong'} = $book->{'TitleLong'} || '';
-			$hit->{'notes'} = $book->{'Notes'} || '';
-			$hit->title( $book->{'Title'} );
-			$hit->url( 'http://isbndb.com/search-all.html?kw='.$book->{'isbn'} );
-			push @{$self->{cache}}, $hit;
-		}
-	}
-	return;
-}
-
-1;
-
-=head1 INSTALLATION
-
-To install this module, run the following commands:
-
-  perl Build.PL
-  ./Build
-  ./Build test
-  ./Build install
+=head2 url_encode
 
 =head1 AUTHOR
 
@@ -136,10 +139,6 @@ your bug as I make changes.
 
 Thanks to isbndb.com for the data that powers this module.
 
-I have no affiliation whatsoever with this website, staff or affiliates.
-
-If you are bored, check out my website: http://www.socklabs.com/
-
 =head1 COPYRIGHT & LICENSE
 
 Copyright 2005 Nick Gerakines, all rights reserved.
@@ -148,5 +147,3 @@ This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
-
-1;
